f180b4134e827996cb4719f052f115180673718b
Dom Henderson <dominic@nethermind.io>
2025-08-27 00:36:08 +0100

    Fix preprocessed trace column offsets

--- LeanZKCircuit/Command/Air/assign_columns.lean
+++ LeanZKCircuit/Command/Air/assign_columns.lean
@@ -3,39 +3,43 @@ import LeanZKCircuit.Command.util
 
 open Lean
 
--- produces a list of corresponding AIR columns and subAIR columns
+-- matches the custom air members onto columns of either the main or preprocessed traces
 def calculate_air_column_assignments
   (defn: AirDefinition)
 : List (String × String) :=
-  (defn.entries.foldl (λ (acc: ℕ × List (String × String)) (x: AirEntry) =>
-    let offset := acc.1
+  (defn.entries.foldl (λ (acc: (ℕ × ℕ) × List (String × String)) (x: AirEntry) =>
+    let main_offset := acc.1.1
+    let preprocessed_offset := acc.1.2
     let assignments := acc.2
     match x with
-      | .column name => (offset + 1, assignments.concat (
-        s!"main (id := 0) (column := {offset})",
-        name
-      ))
+      | .column name => (
+          (main_offset + 1, preprocessed_offset),
+          assignments.concat (
+            s!"main (id := 0) (column := {main_offset})",
+            name
+          )
+        )
       | .main_subair name _ width => (
-          offset + width,
+          (main_offset + width, preprocessed_offset),
           assignments.append (
             (List.range width
               ).map λ index => (
-                s!"main (id := 0) (column := {offset + index})",
+                s!"main (id := 0) (column := {main_offset + index})",
                 s!"{name}.columns (column := {index})"
               )
           )
         )
       | .preprocessed_subair name _ width => (
-          offset + width,
+          (main_offset, preprocessed_offset + width),
           assignments.append (
             (List.range width
               ).map λ index => (
-                s!"preprocessed (column := {offset + index})",
+                s!"preprocessed (column := {preprocessed_offset + index})",
                 s!"{name}.columns (column := {index})"
               )
           )
         )
-  ) (0, [])).2
+  ) ((0, 0), [])).2
 
 def calculate_subair_column_assignments
   (defn: SubAirDefinition)
