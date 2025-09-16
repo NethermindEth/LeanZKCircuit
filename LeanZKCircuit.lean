c6c3cbef708a1023feafffdda14d9c6cb59451a2
Dom Henderson <dom@nethermind.io>
2025-09-16 20:56:03 +0100

    Plonky3

--- LeanZKCircuit.lean
+++ LeanZKCircuit.lean
@@ -15,8 +15,24 @@ import LeanZKCircuit.Command.Air.valid_circuit
 
 import LeanZKCircuit.Command.util
 
+import LeanZKCircuit.Plonky3.Command.Air.Lemmas.base_member_projection
+import LeanZKCircuit.Plonky3.Command.Air.Lemmas.subcircuit_isValid
+
+import LeanZKCircuit.Plonky3.Command.Air.Syntax.air_definition
+import LeanZKCircuit.Plonky3.Command.Air.Syntax.entry
+
+import LeanZKCircuit.Plonky3.Command.Air.assign_columns
+import LeanZKCircuit.Plonky3.Command.Air.define_air
+import LeanZKCircuit.Plonky3.Command.Air.instance_creation
+import LeanZKCircuit.Plonky3.Command.Air.is_valid
+import LeanZKCircuit.Plonky3.Command.Air.structure_definition
+import LeanZKCircuit.Plonky3.Command.Air.valid_circuit
+
+import LeanZKCircuit.Plonky3.Command.util
+
 import LeanZKCircuit.Interactions
 
 import LeanZKCircuit.OpenVM.Circuit
+import LeanZKCircuit.Plonky3.Circuit
 
 import LeanZKCircuit.Tactics.BitVec.bv_amicus_kerneli
