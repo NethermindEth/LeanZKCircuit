67478474c11305ad1ba45ac1dd90b01860edf332
Dom Henderson <dom@nethermind.io>
2025-09-23 19:31:35 +0100

    Include rotation in Plonly3 circuit isValid

--- LeanZKCircuit/Plonky3/Command/Air/Lemmas/subcircuit_isValid.lean
+++ LeanZKCircuit/Plonky3/Command/Air/Lemmas/subcircuit_isValid.lean
@@ -5,9 +5,9 @@ open Lean
 
 namespace Plonky3
   def create_subcircuit_isValid_of_isValid_lemma
-    (circuit: String) (simp_attribute: String) (type_params: String) (member: String) (idx: ℕ) (log : Bool := false)
+    (circuit: String) (simp_attribute: String) (type_params: String) (proof : String) (member: String) (idx: ℕ) (log : Bool := false)
   : Elab.Command.CommandElabM Unit := do
-    let proof := s!"h.1{transformIndex idx}"
+    let proof := s!"{proof}.1{transformIndex idx}"
     let command :=
       s!"@[{simp_attribute}]\n" ++
       s!"lemma Raw_{circuit}.subcircuit_{member}_isValid_of_isValid {"{"}{type_params}{"}"}\n" ++
@@ -31,6 +31,7 @@ namespace Plonky3
           defn.name
           defn.simp_attribute
           "F ExtF"
+          "h.2"
           name
           idx
           log
@@ -49,6 +50,7 @@ namespace Plonky3
           defn.name
           defn.simp_attribute
           "F"
+          "h"
           name
           idx
           log
--- LeanZKCircuit/Plonky3/Command/Air/is_valid.lean
+++ LeanZKCircuit/Plonky3/Command/Air/is_valid.lean
@@ -38,6 +38,11 @@ def define_air_isValid
     s!"def Raw_{defn.name}.isValid {"{"}F ExtF{"}"}\n" ++
     s!"  (c: Raw_{defn.name} F ExtF)\n" ++
     s!": Prop :=\n" ++
+    s!"  (∀ column row rotation,\n" ++
+    s!"    c.main column row rotation = c.main column ((row + rotation) % (c.last_row + 1)) 0 ∧\n" ++
+    s!"    c.permutation column row rotation = c.permutation column ((row + rotation) % (c.last_row + 1)) 0 ∧\n" ++
+    s!"    c.preprocessed column row rotation = c.preprocessed column ((row + rotation) % (c.last_row + 1)) 0\n" ++
+    s!"  ) ∧\n" ++
     s!"  ({subair_term}) ∧\n" ++
     s!"  (∀ row rotation, {columns_term})"
   runAsCommand command log
