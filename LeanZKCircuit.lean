7fa14b086150249f260a76e80c7a2901e19175c0
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-10-13 16:22:37 +0200

    cleanup project superstructur, add vector_eq

--- LeanZKCircuit.lean
+++ LeanZKCircuit.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `LeanZKCircuit` library.
 -- Import modules here that should be built as part of the library.
-import LeanZKCircuit.Basic
+import LeanZKCircuit.Tactics.BitVec.bv_amicus_kerneli
+import LeanZKCircuit.Tactics.Vector.vector_eq
